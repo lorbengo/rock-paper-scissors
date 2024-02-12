db4ccd4d5a9861a23bd500a1747313e337a9b3c5
lorbengo <lorbengo@gmail.com>
2024-02-12 11:34:11 +0300

    Add playGame function

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
-const player = 'ROCK';
-const computer = getComputerChoice();
+let playerScore = 0;
+let computerScore = 0;
 
-console.log(playRound(player, computer));
+playGame();
 
-function playRound(playerSelection, computerSelection) {
-  const ps = playerSelection.toLowerCase();
-  const cs = computerSelection.toLowerCase();
-
-  if (ps === cs) {
-    return 'Tie!'
-  } else if (
-    ps === 'rock' && cs === 'scissors' ||
-    ps === 'paper' && cs === 'rock' ||
-    ps === 'scissors' && cs === 'paper'
-  ) {
-    return `You Win! ${capitalize(ps)} beats ${capitalize(cs)}`;
+function playGame() {
+  for (let round = 1; round <= 5; round++) {
+    const player = prompt(`Round ${round}: Please enter ROCK, PAPER, or SCISSORS`);
+    const computer = getComputerChoice();
+    console.log(playRound(player, computer));
+  }
+  if (playerScore > computerScore) {
+    console.log('The Winner is Player!');
+  } else if (playerScore < computerScore) {
+    console.log('The Winner is Computer!');
   } else {
-    return `You Lose! ${capitalize(cs)} beats ${capitalize(ps)}`;
+    console.log('Tie!');
   }
 }
 
@@ -24,6 +22,24 @@ function getComputerChoice() {
   const choices = ['Rock', 'Paper', 'Scissors'];
   const randNum = Math.floor(Math.random() * 3);
   return choices[randNum];
+}
+
+function playRound(playerSelection, computerSelection) {
+  const ps = playerSelection.toLowerCase();
+  const cs = computerSelection.toLowerCase();
+  if (ps === cs) {
+    return 'Tie!'
+  } else if (
+    ps === 'rock' && cs === 'scissors' ||
+    ps === 'paper' && cs === 'rock' ||
+    ps === 'scissors' && cs === 'paper'
+  ) {
+    playerScore++;
+    return `You Win! ${capitalize(ps)} beats ${capitalize(cs)}`;
+  } else {
+    computerScore++;
+    return `You Lose! ${capitalize(cs)} beats ${capitalize(ps)}`;
+  }
 }
 
 function capitalize(word) {
