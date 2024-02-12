334c0564abc28148da0e3ff0c29c187c80e8ff51
lorbengo <lorbengo@gmail.com>
2024-02-12 10:44:51 +0300

    Add playRound and capitalize functions

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
+const player = 'ROCK';
+const computer = getComputerChoice();
+
+console.log(playRound(player, computer));
+
+function playRound(playerSelection, computerSelection) {
+  const ps = playerSelection.toLowerCase();
+  const cs = computerSelection.toLowerCase();
+
+  if (ps === cs) {
+    return 'Tie!'
+  } else if (
+    ps === 'rock' && cs === 'scissors' ||
+    ps === 'paper' && cs === 'rock' ||
+    ps === 'scissors' && cs === 'paper'
+  ) {
+    return `You Win! ${capitalize(ps)} beats ${capitalize(cs)}`;
+  } else {
+    return `You Lose! ${capitalize(cs)} beats ${capitalize(ps)}`;
+  }
+}
+
 function getComputerChoice() {
   const choices = ['Rock', 'Paper', 'Scissors'];
   const randNum = Math.floor(Math.random() * 3);
-  return choices[randNum]
+  return choices[randNum];
+}
+
+function capitalize(word) {
+  return word[0].toUpperCase() + word.slice(1).toLowerCase();
 }
