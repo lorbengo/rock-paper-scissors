3a813d11dd56eb7ca70ae6d89795cd17748166e7
lorbengo <lorbengo@gmail.com>
2024-02-16 22:42:34 +0300

    Add UI

--- script.js
+++ script.js
@@ -1,47 +1,112 @@
+const score = document.getElementById("score");
+const display = document.getElementById("display");
+const newGameButton = document.querySelector(".new-game");
+const playerSelection = document.getElementById("player-selection");
+const rock = playerSelection.firstElementChild;
+const paper = rock.nextElementSibling;
+const scissors = playerSelection.lastElementChild;
 let playerScore = 0;
 let computerScore = 0;
 
-playGame();
+updateScore();
 
-function playGame() {
-  for (let round = 1; round <= 5; round++) {
-    const player = prompt(`Round ${round}: Please enter ROCK, PAPER, or SCISSORS`);
-    const computer = getComputerChoice();
-    console.log(playRound(player, computer));
+playerSelection.addEventListener("click", playGame);
+playerSelection.addEventListener("keydown", moveFocus);
+newGameButton.addEventListener("click", newGame);
+
+function playGame(event) {
+  const playerChoice = event.target.id;
+  if (display.classList.contains("hidden")) {
+    display.classList.remove("hidden");
   }
-  if (playerScore > computerScore) {
-    console.log('The Winner is Player!');
-  } else if (playerScore < computerScore) {
-    console.log('The Winner is Computer!');
+  playRound(playerChoice, getComputerChoice());
+  if (playerScore === 5 || computerScore === 5) {
+    if (playerScore === 5) {
+      display.innerHTML =
+        "<p>Game Over!</p><p>The Winner is Player! 🎉🥳👏</p>";
+    } else {
+      display.innerHTML =
+        "<p>Game Over!</p><p>The Winner is... Computer! 🤦‍♂️🤦🤦‍♀️🙈</p>";
+    }
+    disablePlayerSelectionButtons();
+    newGameButton.classList.toggle("new-game");
+  }
+}
+
+function moveFocus(event) {
+  if (event.key === "ArrowRight" || event.key === "ArrowDown") {
+    if (event.target.id === "rock") {
+      paper.focus();
+    } else if (event.target.id === "paper") {
+      scissors.focus();
+    } else if (event.target.id === "scissors") {
+      rock.focus();
+    }
+  } else if (event.key === "ArrowLeft" || event.key === "ArrowUp") {
+    if (event.target.id === "scissors") {
+      paper.focus();
+    } else if (event.target.id === "paper") {
+      rock.focus();
+    } else if (event.target.id === "rock") {
+      scissors.focus();
+    }
+  }
+}
+
+function newGame() {
+  newGameButton.classList.add("new-game");
+  display.textContent = "";
+  display.classList.add("hidden");
+  playerScore = 0;
+  computerScore = 0;
+  updateScore();
+  enablePlayerSelectionButtons();
+}
+
+function updateScore() {
+  score.firstElementChild.textContent = playerScore;
+  score.lastElementChild.textContent = computerScore;
+}
+
+function playRound(player, computer) {
+  if (player === computer) {
+    display.textContent = "Tie!";
+  } else if (
+    (player === "rock" && computer === "scissors") ||
+    (player === "paper" && computer === "rock") ||
+    (player === "scissors" && computer === "paper")
+  ) {
+    playerScore++;
+    display.textContent = `You Win! ${capitalize(player)} beats ${capitalize(
+      computer,
+    )}`;
   } else {
-    console.log('Tie!');
+    computerScore++;
+    display.textContent = `You Lose! ${capitalize(computer)} beats ${capitalize(
+      player,
+    )}`;
+  }
+  updateScore();
+}
+
+function disablePlayerSelectionButtons() {
+  for (let item of playerSelection.children) {
+    item.disabled = true;
+  }
+}
+
+function enablePlayerSelectionButtons() {
+  for (let item of playerSelection.children) {
+    item.disabled = false;
   }
 }
 
 function getComputerChoice() {
-  const choices = ['Rock', 'Paper', 'Scissors'];
+  const choices = ["rock", "paper", "scissors"];
   const randNum = Math.floor(Math.random() * 3);
   return choices[randNum];
 }
 
-function playRound(playerSelection, computerSelection) {
-  const ps = playerSelection.toLowerCase();
-  const cs = computerSelection.toLowerCase();
-  if (ps === cs) {
-    return 'Tie!'
-  } else if (
-    ps === 'rock' && cs === 'scissors' ||
-    ps === 'paper' && cs === 'rock' ||
-    ps === 'scissors' && cs === 'paper'
-  ) {
-    playerScore++;
-    return `You Win! ${capitalize(ps)} beats ${capitalize(cs)}`;
-  } else {
-    computerScore++;
-    return `You Lose! ${capitalize(cs)} beats ${capitalize(ps)}`;
-  }
-}
-
 function capitalize(word) {
-  return word[0].toUpperCase() + word.slice(1).toLowerCase();
+  return word[0].toUpperCase() + word.slice(1);
 }
